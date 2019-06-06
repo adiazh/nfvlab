95e0cec1328858a13b7d3d1360cb39417a4ad95f
Pedro A. Aranda Gutierrez <paranda@it.uc3m.es>
2019-06-06 18:00:00 +0200

    ok menos httpd

--- DevelNFV/deploy.sh
+++ DevelNFV/deploy.sh
@@ -1,10 +1,5 @@
 echo "deploying descriptiors in OSM" 
 
-# osm vnfd-create webserver_vnf.tar.gz 
-# osm --hostname 192.168.100.24 vnfd-create web_vnf_vnfd.tar.gz 
-osm --hostname 192.168.100.24 vnfd-create webserver_vnf.tar.gz 
+osm vnfd-create webserver_vnf.tar.gz 
 
-#osm nsd-create multi-VM_ns.tar.gz
-#osm nsd-create mono-VM_ns.tar.gz
-#osm --hostname 192.168.100.24 nsd-create singleWebServer_ns_nsd.tar.gz
-osm --hostname 192.168.100.24 nsd-create mono-VM_ns.tar.gz
+osm nsd-create mono-VM_ns.tar.gz
